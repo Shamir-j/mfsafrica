610cfc6db4f5bbea3ee49519fcff867baf4bc35b
Gideon Okwach <gideonochiengokwach@gmail.com>
2021-08-12 23:41:24 +0300

    upadted code

--- closetpoint/settings.py
+++ closetpoint/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
-# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 # CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = ['198.211.99.20', 'localhost', '127.0.0.1'] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 # CORS_ALLOWED_ORIGIN_REGEXES =['198.211.99.20', 'localhost', '127.0.0.1']
